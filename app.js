525119b6933fab6f4f831be8300ed885ce969c72
Chadin Chaipornpisuth <ping47024@icloud.com>
2024-10-22 21:46:31 +0700

    first pack

---
+++ app.js
@@ -0,0 +1,22 @@
+const n = '<br />'
+const w = (str) => document.write(str+n)
+w(VERSION)
+w(ABT('123'))
+w(BT('456'))
+w(BF('123'))
+w(LNBT("Million"));
+w(JSON.stringify(FTHAISATANGWORDS));
+w(JSON.stringify(LTHAISATANGWORDS));
+w(JSON.stringify(ONETONINE));
+w(JSON.stringify(THAI2ARABICNumerals));
+w(JSON.stringify(THAINUMBERWORDS));
+w(JSON.stringify(REVERSETHAIDIGITWORDS));
+w(JSON.stringify(MILLION));
+w(repeat(".",[99]))
+w(TB("หนึ่งล้านบาทถ้วน"));
+w(TB("สิบล้านบาทถ้วน"));
+w(TB("หนึ่งร้อยล้านบาทถ้วน"));
+w(TB("หนึ่งพันล้านบาทถ้วน"));
+w(TB("หนึ่งหมื่นล้านบาทถ้วน"));
+w(TB("หนึ่งแสนล้านบาทถ้วน"));
+w(TB("หนึ่งล้านล้านบาทถ้วน"));
